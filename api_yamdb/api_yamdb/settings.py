2b75c92afb560491c6fb1c4951ac77a1e3805a45
Pavel Gaidukov <mailgpn@gmail.com>
2022-08-07 20:17:22 +0500

    update setings adden default values

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', , default='mysecretkey12312312312')
+SECRET_KEY = os.getenv('SECRET_KEY', default='mysecretkey12312312312')
 
 DEBUG = False
 
